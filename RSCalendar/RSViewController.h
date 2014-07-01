2d593b65b2410b25f4ff7990bd8d152da634a835
Ross Simmerman <rosssimmerman@Rosss-MacBook-Air.local>
2014-06-30 21:55:54 -0400

    Initial Commit

---
+++ RSCalendar/RSViewController.h
@@ -0,0 +1,13 @@
+//
+//  RSViewController.h
+//  RSCalendar
+//
+//  Created by Ross Simmerman on 6/30/14.
+//  Copyright (c) 2014 RossSimmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSViewController : UIViewController
+
+@end
---
+++ RSCalendarTests/RSCalendarTests.m
@@ -0,0 +1,34 @@
+//
+//  RSCalendarTests.m
+//  RSCalendarTests
+//
+//  Created by Ross Simmerman on 6/30/14.
+//  Copyright (c) 2014 RossSimmerman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RSCalendarTests : XCTestCase
+
+@end
+
+@implementation RSCalendarTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
